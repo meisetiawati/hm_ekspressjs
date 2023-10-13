fb64c6908de9fb0033943627ad570da79cb78558
Mei Setiawaty <meisetiawati>
2023-10-13 22:42:41 +0800

    Express js & Integrasi database

--- server/index.js
+++ server/index.js
@@ -1,12 +1,65 @@
 const express = require('express');
-const sequelize = require('sequelize');
-const film = sequelize.define('./router/film');
-const Category = sequelize.define('./router/category');
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('dvdrental','root','', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
 const app = express();
 const port = 3000;
-const sequelize = new sequelize('dvdrental','root','', {
-    host: 'localhost',
-    dialect: 'mysql',
+
+const film = sequelize.define('./router/film', {
+    film_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+    },
+    title: {
+        type: DataTypes.STRING
+    },
+    description: {
+        type: DataTypes.TEXT
+    },
+    release_year: {
+        type: DataTypes.DATE
+    },
+    language_id: {
+        type: DataTypes.INTEGER
+    },
+    rental_rate: {
+        type: DataTypes.DECIMAL
+    },
+    length: {
+        type: DataTypes.INTEGER
+    },
+    replacement_cost: {
+        type: DataTypes.DECIMAL
+    },
+    rating: {
+        type: DataTypes.STRING
+    },
+    last_update: {
+        type: DataTypes.DATE
+    },
+    special_features: {
+        type: DataTypes.TEXT
+    },
+    full_text: {
+        type: DataTypes.STRING
+    },
+
+});
+
+const Category = sequelize.define('./router/category', {
+    category_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING
+    },
+    last_update: {
+        type: DataTypes.DATE
+    },
+
 });
 
 
@@ -16,7 +69,7 @@ app.get('/film', async(req, res) => {
 });
 
 app.get('/film/:film_id', async(req, res) => {
-    const film = await film.findByPk(reg.params.id);
+    const film = await film.findByPk(reg.params.film_id);
     res.json(film);
 });
 
@@ -27,7 +80,7 @@ app.get('/category', async(req, res) => {
 
 app.get('/film/category/:category', async(req, res) => {
     const film = await film.findAll({
-        where: {category_id: reg.params.category},
+        where: {category_id: reg.params.category}
     });
     res.json(film);
 });
